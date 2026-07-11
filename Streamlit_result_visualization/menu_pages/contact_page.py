d5eb367902cfd093e6941750ad815593d91f7406
Yosef Ahmed Shabaan <yosefshabaan05@gmail.com>
2026-07-11 15:50:37 +0300

    chore: clear contact page names

--- Streamlit_result_visualization/menu_pages/contact_page.py
+++ Streamlit_result_visualization/menu_pages/contact_page.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-# Funzione per la home page
+
 def contact_page():
-    st.title("Team Members:")
-    st.write(f"#### - Argentino Simone   - M63001555")
-    st.write(f"#### - Romano Vincenzo    - M63001557")
-    st.write(f"#### - Borrelli Francesco - M63001559")
-    
+    st.title("Contact Us")
